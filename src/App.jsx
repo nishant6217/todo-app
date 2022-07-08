0c13536886786ee1dff861fad6ee398df4ae620c
Nishant <nishant.kumar@sheshi.ai>
2022-07-08 14:13:57 +0530

    fix: code allignment fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,44 @@
 import React, { useState } from "react";
 import TodoList from "./TodoList";
 
-const App = () =>{
-
+const App = () => {
     const [inputList, setInputList] = useState("");
     const [items, setItems] = useState([]);
 
-
-    const itemEvent =(event) =>{
+    const itemEvent = (event) => {
         setInputList(event.target.value);
     };
 
-    const listOfItems =() =>{
-setItems((oldItems) =>{
-    return [...oldItems, inputList]
-})
-setInputList('')
+    const listOfItems = () => {
+        setItems((oldItems) => {
+            return [...oldItems, inputList]
+        })
+        setInputList('')
     };
 
-    return(
+    return (
         <>
-        <div className="main_div">
+            <div className="main_div">
 
-            <div className="center_div">
-                <br />
-                <h1> Todo List</h1>
-                <br />
-                <input type="text" placeholder ="Add an Item" onChange={itemEvent} value={inputList}/>
-                <button onClick={listOfItems}>+</button>
-                <ol>
-                    {/* <li>{inputList}</li> */}
-                    {items.map((itemval)=>{
-                        // return <li>{itemval}</li>
-                         return <TodoList text ={itemval} />
-                    })}
-                </ol>
+                <div className="center_div">
+                    <br />
+                    <h1> Todo List</h1>
+                    <br />
+                    <input type="text" placeholder="Add an Item" onChange={itemEvent} value={inputList} />
+                    <button onClick={listOfItems}>+</button>
+                    <ol>
+                        {/* <li>{inputList}</li> */}
+                        {items.map((itemval) => {
+                            // return <li>{itemval}</li>
+                            return <TodoList text={itemval} />
+                        })}
+                    </ol>
+                </div>
             </div>
-        </div>
-        
-        
+
+
         </>
     )
-     
+
 }
 export default App;
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const TodoList =(props) =>{
+const TodoList = (props) => {
     return (
-    <>
-    <div className="todo_style">
-    <li> {props.text } </li>
-    </div>
-    </>
+        <>
+            <div className="todo_style">
+                <li> {props.text} </li>
+            </div>
+        </>
     )
 };
 
